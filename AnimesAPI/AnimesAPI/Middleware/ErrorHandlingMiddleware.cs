6e3680b241149f8edd9cf4c74fd52f103f3b3624
WalterHenri <walterdoideira5438@gmail.com>
2025-07-01 21:14:07 -0300

    feat(api): Adicionado Middleware para erros

---
+++ AnimesAPI/AnimesAPI/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace AnimesAPI.Middleware
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+
+        public ErrorHandlingMiddleware(RequestDelegate next, ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Ocorreu um erro inesperado.");
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var code = HttpStatusCode.InternalServerError;
+            var result = string.Empty;
+
+            switch (exception)
+            {
+                case NotFoundException notFoundException:
+                    code = HttpStatusCode.NotFound;
+                    result = JsonSerializer.Serialize(new { error = notFoundException.Message });
+                    break;
+                // Adicione outros casos para exceções customizadas aqui
+                default:
+                    result = JsonSerializer.Serialize(new { error = "Ocorreu um erro interno no servidor." });
+                    break;
+            }
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
--- AnimesAPI/AnimesAPI/Program.cs
+++ AnimesAPI/AnimesAPI/Program.cs
@@ -1,13 +1,14 @@
 using Animes.Application.Features.Animes.Queries;
 using Animes.Application.Mappers;
 using Animes.Domain.Interfaces.Repositories;
-using Microsoft.EntityFrameworkCore.InMemory;
 using Animes.Infrastructure.Persistence;
 using Animes.Infrastructure.Persistence.Contexts;
 using Animes.Infrastructure.Persistence.Repositories;
+using AnimesAPI.Middleware;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
 using Microsoft.Identity.Web;
 using Microsoft.OpenApi.Models;
 using System.Reflection;
@@ -63,6 +64,8 @@ public class Program
         });
 
         var app = builder.Build();
+
+        app.UseMiddleware<ErrorHandlingMiddleware>();
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
